28e4afb4b8fc6b3f6b1bd9f3ace6e048e372e8d2
0hee0 <suhee0206@gmail.com>
2021-07-20 17:24:27 +0900

    Add layout & Implement Filters

---
+++ boilerplate/client/src/components/views/StorePage/Sections/RadioBox.js
@@ -0,0 +1,31 @@
+import React, { useState } from 'react';
+import { Radio, Card } from 'antd';
+
+function RadioBox(props) {
+    const [value, setValue] = useState("")
+
+    const renderRadioBox = () => (
+        props.list && props.list.map((value) => (
+            <Radio.Button key={value._id} value={value.name}> {value.name} </Radio.Button>
+        ))
+    )    
+
+    const handleChange = (event) => {
+        setValue(event.target.value)
+        props.handleFilters(event.target.value)
+    }
+
+    return (
+        <div>
+            <Card title="Category">
+                
+                <Radio.Group onChange={handleChange} value={value} buttonStyle="solid">
+                    {renderRadioBox()}                        
+                </Radio.Group>
+
+            </Card>
+        </div>
+    )
+}
+
+export default RadioBox

42bd555626a556bbb00c552a2f65a2e063d2a73e
0hee0 <suhee0206@gmail.com>
2021-07-29 01:22:55 +0900

    Implement ReviewTab & InfoTab

--- boilerplate/client/src/components/views/ProductPage/Sections/InfoTab.js
+++ boilerplate/client/src/components/views/ProductPage/Sections/InfoTab.js
@@ -1,10 +1,83 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react';
+import { Card, Descriptions, Typography, Row } from 'antd';
+import axios from 'axios';
+
+const { Text } = Typography
 
 function InfoTab(props) {
+    const [storeInfo, setStoreInfo] = useState()
+
+    useEffect(() => {
+        let body = { 
+            store: props.storeId
+        }
+
+        axios.post(`/api/stores/getStoreInfo`, body)
+        .then(response => {
+            console.log("response"+JSON.stringify(response.data.storeInfo[0]))
+            setStoreInfo(response.data.storeInfo[0])
+        })
+        .catch(err => alert(err))
+    }, [])
+
     return (
-        <div>
-            정보
-        </div>
+        storeInfo ? (
+            <div>
+                <Row gutter={16}>
+                    <Card title="업체정보">
+                        <Descriptions>
+                            <Descriptions.Item label="영업시간">{storeInfo.hour.open} - {storeInfo.hour.close}</Descriptions.Item>
+                            <Descriptions.Item label="휴무일">{storeInfo.dayoff}</Descriptions.Item>
+                            <Descriptions.Item label="전화번호">{storeInfo.contact}</Descriptions.Item>
+                            <Descriptions.Item label="주소">{storeInfo.storeAddress.address_name}</Descriptions.Item>
+                        </Descriptions>
+                    </Card>
+                </Row>
+                <Row gutter={16}>
+                    <Card title="위생정보">
+                        <Descriptions>
+                            <Descriptions.Item>
+                                {storeInfo.sanitary ? 
+                                    <Text mark>위생인증가게</Text>
+                                    :
+                                    <Text disabled>X</Text>
+                                }
+                            </Descriptions.Item>
+                        </Descriptions>
+                    </Card>
+                </Row>
+                <Row gutter={16}>
+                    <Card title="사업자정보">
+                        <Descriptions>
+                            <Descriptions.Item label="사업자명">
+                                {storeInfo.storeOwner.ownerName}
+                            </Descriptions.Item>
+                            <Descriptions.Item label="상호명">
+                                {storeInfo.storeOwner.tradeName}
+                            </Descriptions.Item>
+                            <Descriptions.Item label="사업자등록번호">
+                                {storeInfo.storeOwner.businessRegistrationNumber}
+                            </Descriptions.Item>
+                        </Descriptions>
+                    </Card>
+                </Row>
+                <Row gutter={16}>
+                    <Card title="원산지정보">
+                        <Descriptions>
+                            <Descriptions.Item>
+                                원산지정보
+                            </Descriptions.Item>
+                        </Descriptions>
+                    </Card>
+                </Row>
+            </div>
+        )
+        :
+        (
+            <div>
+                업체정보
+            </div>
+        )
     )
 }
 
